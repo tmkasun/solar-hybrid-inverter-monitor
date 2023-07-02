d0ff4059195cec385c201471e618f95486f995cb
tmkasun <tmkasun@gmail.com>
2023-07-02 19:47:39 +0000

    fix int errors

--- inverter-api/libs/parsers.py
+++ inverter-api/libs/parsers.py
@@ -1,6 +1,7 @@
 ACK = "ACK"
 NACK = "NAK"
 
+
 def parseQPIWS(responseString):
     paddingRemoved = responseString[1:]
     if len(paddingRemoved) >= 28:
@@ -37,6 +38,7 @@ def parseQPIWS(responseString):
     else:
         return {}
 
+
 # Setting device output source priority
 
 
@@ -56,22 +58,26 @@ def parsePCP(responseString):
         return {"accept": True}
     if paddingRemoved.startswith(NACK):
         return {"accept": False}
-    return {"raw": '{}'.format(responseString)}
+    return {"raw": "{}".format(responseString)}
 
 
 def parseQMOD(responseString):
-    if responseString[1].lower() == 'b':
-        return {'mode': 'Battery'}
-    if responseString[1].lower() == 'l':
-        return {'mode': 'Line'}
-    if responseString[1].lower() == 's':
-        return {'mode': 'Standby'}
-    if responseString[1].lower() == 'f':
-        return {'mode': 'Fault'}
-    if responseString[1].lower() == 'y':
-        return {'mode': 'Bypass'}
-    else:
-        return {'raw': '{}'.format(responseString)}
+    try:
+        if responseString[1].lower() == "b":
+            return {"mode": "Battery"}
+        if responseString[1].lower() == "l":
+            return {"mode": "Line"}
+        if responseString[1].lower() == "s":
+            return {"mode": "Standby"}
+        if responseString[1].lower() == "f":
+            return {"mode": "Fault"}
+        if responseString[1].lower() == "y":
+            return {"mode": "Bypass"}
+        else:
+            return {"raw": "{}".format(responseString)}
+    except:
+        print("Error while parsing QMOD")
+        return {"mode": "Unknow"}
 
 
 """
@@ -83,32 +89,32 @@ def parseQPIGS(responseString):
     # print(responseString)
     # print(len(responseString))
     # assert False
-    inverterParameters = responseString.split(' ')
+    inverterParameters = responseString.split(" ")
     # print(len(inverterParameters))
 
     parameterMappings = [
-        'Grid Voltage',
-        'Grid Frequency',
-        'Output Voltage',
-        'Output Frequency',
-        'Output VA',
-        'Output Power(W)',
-        'Output load %',
-        'BUS Voltage',
-        'Battery Voltage',
-        'Battery Charging Current',
-        'Battery capacity',
-        'Inverter heat sink temperature',
-        'PV Input current for battery',
-        'PV Voltage',
-        'Battery V from SCC',
-        'Battery discharge I',
-        'Device status',
-        'Fans voltage offset(mV)',
-        'EEPROM version',
-        'PV Power',
-        'Device status 2',
-        'Unknow',
+        "Grid Voltage",
+        "Grid Frequency",
+        "Output Voltage",
+        "Output Frequency",
+        "Output VA",
+        "Output Power(W)",
+        "Output load %",
+        "BUS Voltage",
+        "Battery Voltage",
+        "Battery Charging Current",
+        "Battery capacity",
+        "Inverter heat sink temperature",
+        "PV Input current for battery",
+        "PV Voltage",
+        "Battery V from SCC",
+        "Battery discharge I",
+        "Device status",
+        "Fans voltage offset(mV)",
+        "EEPROM version",
+        "PV Power",
+        "Device status 2",
+        "Unknow",
     ]
     currentIndex = 0
     data = {}
@@ -120,56 +126,26 @@ def parseQPIGS(responseString):
         if currentIndex == 16:
             # 8 bit status flags
             deviceStatus = {
-                'pvoac': {
-                    "name": "PV or AC feed the load",
-                    "value": parameter[0]
-                },
-                'confs': {
-                    "name": "Configuration status",
-                    "value": parameter[1]
-                },
-                'sccfwv': {
-                    "name": "SCC firmware version",
-                    "value": parameter[2]
-                },
-                'ls': {
-                    "name": "Load status",
-                    "value": parameter[3]
-                },
-                'res': {
-                    "name": "reserved",
-                    "value": parameter[4]
-                },
-                'conof': {
-                    "name": "Charging on/off",
-                    "value": parameter[5]
-                },
-                'sccconof': {
-                    "name": "SCC charging on/off",
-                    "value": parameter[6]
-                },
-                'acconof': {
-                    "name": "AC charging on/off",
-                    "value": parameter[7]
-                }
+                "pvoac": {"name": "PV or AC feed the load", "value": parameter[0]},
+                "confs": {"name": "Configuration status", "value": parameter[1]},
+                "sccfwv": {"name": "SCC firmware version", "value": parameter[2]},
+                "ls": {"name": "Load status", "value": parameter[3]},
+                "res": {"name": "reserved", "value": parameter[4]},
+                "conof": {"name": "Charging on/off", "value": parameter[5]},
+                "sccconof": {"name": "SCC charging on/off", "value": parameter[6]},
+                "acconof": {"name": "AC charging on/off", "value": parameter[7]},
             }
             parameter = deviceStatus
         elif currentIndex == 20:
             deviceStatus = {
-                'fmode': {
-                    'name': 'charging to floating mode',
-                    "value": parameter[0]
-                },
-                'sw': {
-                    'name': 'Switch On',
-                    "value": parameter[1]
-                },
-                'dustp': {
-                    'name': 'dustproof installed',
-                    "value": parameter[2]
-                }
+                "fmode": {"name": "charging to floating mode", "value": parameter[0]},
+                "sw": {"name": "Switch On", "value": parameter[1]},
+                "dustp": {"name": "dustproof installed", "value": parameter[2]},
             }
             parameter = deviceStatus
-        data[parameterMappings[currentIndex]] = parameter
+        try:
+            data[parameterMappings[currentIndex]] = parameter
+        except:
+            print("Error occored while parsing!")
         currentIndex += 1
     return data
---
+++ inverter-api/run.jkbms.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "JK BMS: Use -o json for json output. This will take about 10-15 seconds . . . "
+ jkbms -p C8:47:8C:E2:A0:2E -n "JK-B1A20S15P" -P JK02 -c getCellData
